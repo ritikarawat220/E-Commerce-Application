176ea4d9a51af58413cac527ccc8000d90a91454
Ritika Rawat <Ritikarawat220@gmail.com>
2023-08-15 14:00:07 +0530

    Add components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Amazon from "./components/amazon";
+import Navbar from "./components/navbar";
+import Cart from "./components/cart";
 
-function App() {
+const App = () => {
+  const [show, setShow] = useState(true);
+  const [cart, setCart] = useState([]);
+
+  const handleClick = (item) => {
+    if (cart.indexOf(item) !== -1) return;
+    setCart([...cart, item]);
+  };
+
+  const handleChange = (item, d) => {
+    const ind = cart.indexOf(item);
+    const arr = cart;
+    arr[ind].amount += d;
+
+    if (arr[ind].amount === 0) arr[ind].amount = 1;
+    setCart([...arr]);
+  };
+
+  // useEffect(() => {
+  //   console.log("cart change");
+  // }, [cart]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <Navbar setShow={setShow} size={cart.length} />
+      {show ? (
+        <Amazon handleClick={handleClick} />
+      ) : (
+        <Cart cart={cart} setCart={setCart} handleChange={handleChange} />
+      )}
+    </React.Fragment>
   );
-}
+};
 
 export default App;
